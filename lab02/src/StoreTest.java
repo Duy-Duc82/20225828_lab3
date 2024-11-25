e4d06db46bbe623031954d6932601a22f76bd3fb
Duy-Duc82 <duongducduy825@gmail.com>
2024-11-25 23:49:56 +0700

    bai 7

---
+++ lab02/src/StoreTest.java
@@ -0,0 +1,28 @@
+import lab02.src.Store;
+import lab02.src.DigitalVideoDisc;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        // Tạo cửa hàng
+        Store store = new Store();
+
+        // Tạo các DVD mới
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", 18.99f);
+
+        // Thêm DVD vào cửa hàng
+        store.addDVD(dvd1);
+        store.addDVD(dvd2);
+        store.addDVD(dvd3);
+
+        // In danh sách DVD trong cửa hàng
+        store.printStoreDetails();
+
+        // Xóa DVD khỏi cửa hàng
+        store.removeDVD(dvd2);
+
+        // In danh sách DVD sau khi xóa
+        store.printStoreDetails();
+    }
+}
